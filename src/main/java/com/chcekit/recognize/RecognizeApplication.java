b2f05ad1f6e2eff5d6af2ca610765b7176c465ff
Angular CLI <angular-cli@angular.io>
2018-04-02 13:45:34 +0200

    chore: initial commit from @angular/cli

---
+++ src/main/java/com/chcekit/recognize/RecognizeApplication.java
@@ -0,0 +1,12 @@
+package com.chcekit.recognize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecognizeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecognizeApplication.class, args);
+    }
+}
